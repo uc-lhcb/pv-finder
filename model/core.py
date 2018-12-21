f4d95d4145763d3addee21494d62d5de87961798
Henry Schreiner <hschrein@cern.ch>
2018-12-21 10:19:18 -0500

    Adding softplus, some better plotting tools

--- model/core.py
+++ model/core.py
@@ -9,26 +9,30 @@ class PVModel(nn.Module):
     DEFAULTS = {} # Can set default dropouts if desired
     FC = True # Can turn off fully connected linear output layer
     INPUTS = 1 # Number of inputs (1 for z only, or 3 for x and y too)
-    
+    FINAL_ACTIVATION = nn.Sigmoid
+
     def __init__(self, **kwargs):
         """Make a PV layer model.
-        
+
         You can pass dropout_<x> = value to set dropout for a layer. Layers start at 1.
+        You can pass final_activation or set FINAL_ACTIVATION to set the final activation function.
         """
         super().__init__()
-        
+
         options = {**self.DEFAULTS, **kwargs}
-        
+
+        self.final_activation = options['final_activation'] if 'final_activation' in options else self.FINAL_ACTIVATION
+
         nlayers = len(self.KERNEL_SIZE)
         assert len(self.CHANNELS_SIZE) == nlayers, "You need as many channels as layers"
         assert all(v % 2 == 1 for v in self.KERNEL_SIZE), "All kernel sizes must be odd"
-        
+
         # Make local names to use. Add 1 before and after the channels size.
         kernel_size = self.KERNEL_SIZE
         channels_size = (self.INPUTS, *self.CHANNELS_SIZE)
-        
+
         leaky = nn.LeakyReLU(self.LEAKYNESS)
-        
+
         # Build up sequential layers
         items = []
         for i in range(nlayers):
@@ -41,46 +45,46 @@ class PVModel(nn.Module):
                     padding = (kernel_size[i] - 1) // 2
                 )
             )
-            
+
             # LeakyReLu
-            items.append(leaky if self.FC or i<nlayers-1 else nn.Sigmoid())
-            
+            items.append(leaky if self.FC or i<nlayers-1 else self.final_activation)
+
             # Get dropout if passed in and not none, and add that
             dropout = options.get(f"dropout_{i+1}")
             if dropout is not None:
                 items.append(nn.Dropout(dropout))
-            
+
         self.features = nn.Sequential(*items)
-        
+
         if self.FC:
             self.fc = nn.Linear(
                 in_features = 4000*channels_size[-1],
                 out_features = 4000
             )
-        
+
     def forward(self, x):
         x = self.features(x)
-        
+
         # Remove empty middle shape diminsion
         x = x.view(x.shape[0], x.shape[-1])
 
         if self.FC:
-            x = torch.sigmoid(self.fc(x))
+            x = self.final_activation()(self.fc(x))
 
         return x
-    
+
 def write_model(filename, model, loss=None):
     "Write a model and maybe a loss function to a file"
     info_file = filename
-    
+
     master_model = PVModel if issubclass(model, PVModel) else model
-    
+
     with open(info_file, "w") as f:
         print("import torch", file=f)
         print("from torch import nn\n", file=f)
-    
+
         print(inspect.getsource(master_model), end="\n\n\n", file=f)
-        
+
         if issubclass(model, PVModel):
             print(f"""
 class Model(PVModel):
@@ -90,20 +94,21 @@ class Model(PVModel):
     FC = {model.FC}
     LEAKYNESS = {model.LEAKYNESS}
     INPUTS = {model.INPUTS}
+    FINAL_ACTIVATION = {model.FINAL_ACTIVATION.__qualname__}
 """, file=f)
-        
+
         if loss is not None:
             print(inspect.getsource(loss), file=f)
-            
+
 def modernize(d, layers = 2):
     'Convert old style files to the new style'
-    
-    # Factor for layers is 2 or 3
-    
+
+    # Factor for layers is 2 or 3, depending on dropout.
+
     # Convert new style only
     if 'fc1.weight' not in d:
         return d
-    
+
 
     for key in list(d.keys()):
         new_key = None
@@ -114,5 +119,5 @@ def modernize(d, layers = 2):
             new_key = 'fc' + key[3:]
         if new_key:
             d[new_key] = d.pop(key)
-            
+
     return d
--- model/models.py
+++ model/models.py
@@ -11,47 +11,53 @@ class SimpleCNN3Layer(PVModel):
     KERNEL_SIZE =   [25, 15, 5]
     CHANNELS_SIZE = [10,  5, 1]
     DEFAULTS = {'dropout_3':0.35}
-    
+
 class SimpleCNN3Layer_A(PVModel):
     KERNEL_SIZE =   [25, 15, 5]
     CHANNELS_SIZE = [20, 5, 1]
     DEFAULTS = {'dropout_1':0.15, 'dropout_3':0.35}
-    
+
 class SimpleCNN3Layer_B(PVModel):
     KERNEL_SIZE =   [25, 15, 5]
     CHANNELS_SIZE = [20, 10, 1]
     DEFAULTS = {'dropout_1':0.15, 'dropout_2':0.15, 'dropout_3':0.35}
-    
+
 class SimpleCNN3Layer_C(PVModel):
     KERNEL_SIZE =   [25, 15, 5]
     CHANNELS_SIZE = [20, 10, 1]
     DEFAULTS = {'dropout_1':0.15, 'dropout_2':0.15, 'dropout_3':0.35}
-    
+
 class All_CNN3Layer_C(PVModel):
     KERNEL_SIZE =   [25, 15, 5]
     CHANNELS_SIZE = [20, 10, 1]
     DEFAULTS = {'dropout_1':0.15, 'dropout_2':0.15, 'dropout_3':0.35}
-    
+
 class SimpleCNN4Layer_C(PVModel):
     KERNEL_SIZE =   [25, 15, 15, 5]
     CHANNELS_SIZE = [20, 10, 10, 1]
     DEFAULTS = {'dropout_1':0.15, 'dropout_2':0.15, 'dropout_3':0.15, 'dropout_4':0.35}
-    
+
 class SimpleCNN4Layer_D(PVModel):
     KERNEL_SIZE =   [25, 15, 15, 5]
     CHANNELS_SIZE = [25, 25, 25, 1]
     DEFAULTS = {'dropout_1':0.55, 'dropout_2':0.55, 'dropout_3':0.55, 'dropout_4':0.35}
-    
+
 class SimpleCNN4Layer_D35(PVModel):
     KERNEL_SIZE =   [25, 15, 15, 5]
     CHANNELS_SIZE = [25, 25, 25, 1]
     DEFAULTS = {'dropout_1':0.35, 'dropout_2':0.35, 'dropout_3':0.35, 'dropout_4':0.35}
-    
+
+class SimpleCNN4Layer_D35_sp(PVModel):
+    KERNEL_SIZE =   [25, 15, 15, 5]
+    CHANNELS_SIZE = [25, 25, 25, 1]
+    DEFAULTS = {'dropout_1':0.35, 'dropout_2':0.35, 'dropout_3':0.35, 'dropout_4':0.35}
+    FINAL_ACTIVATION = nn.Softplus
+
 class SimpleCNN4Layer_D25(PVModel):
     KERNEL_SIZE =   [25, 15, 15, 5]
     CHANNELS_SIZE = [25, 25, 25, 1]
     DEFAULTS = {'dropout_1':0.25, 'dropout_2':0.25, 'dropout_3':0.25, 'dropout_4':0.25}
-    
+
 class SimpleCNN5Layer_C(PVModel):
     KERNEL_SIZE =   [25, 15, 15, 15, 5]
     CHANNELS_SIZE = [20, 10, 10, 10, 1]
