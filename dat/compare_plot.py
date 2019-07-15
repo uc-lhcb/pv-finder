f3767ac0e6bdf3264e5c8ad7c75896d43b391af2
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-07-15 15:16:42 -0400

    Adding compare script

---
+++ dat/compare_plot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import uproot
+import numpy as np
+import sys
+from pathlib import Path
+import numpy as np
+import matplotlib
+
+DIR = Path(__file__).resolve().parent
+
+def compare(p: Path, orig: Path):
+    f_new = uproot.open(p)['kernel']
+    f_old = uproot.open(orig)['kernel']
+
+    key = 'zdata'
+    d_new = f_new[key].array()
+    d_old = f_old[key].array()
+
+    return plot(d_new, d_old, 2000)
+
+def plot(n: np.ndarray, o: np.ndarray, v: int):
+    fig, axs = plt.subplots(2, 1, figsize=(15,2), sharex=True)
+    axs[0].imshow(n[:,v:v+200])
+    axs[1].imshow(o[:,v:v+200])
+    return fig, axs
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Compare a file with the orinal kernel (10 event run)')
+    parser.add_argument('file', type=Path, help='A ROOT file with a new run')
+    parser.add_argument('--orig', type=Path, default=DIR / 'result_10pvs.root', help='A ROOT file with the old run')
+    parser.add_argument('--save', type=Path, help='A file to write to instead of a display')
+    args = parser.parse_args()
+
+    if args.save:
+        matplotlib.use('Agg')
+
+
+    import matplotlib.pyplot as plt
+    fig, _ = compare(args.file, args.orig)
+    if args.save:
+        plt.savefig(args.save)
+    else:
+        plt.show()
