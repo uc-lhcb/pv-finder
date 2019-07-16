caeda0fb39aac13af947a7a2017144f968f44970
Henry Schreiner <hschrein@cern.ch>
2019-07-16 12:42:17 -0400

    Adding CMake build

---
+++ ana/main.C
@@ -0,0 +1,9 @@
+#include <TString.h>
+
+void makehist(TString input, TString folder = "/data/schreihf/PvFinder");
+
+
+int main(int argc, char** argv) {
+    makehist("10pvs", "../../dat");
+    
+}
