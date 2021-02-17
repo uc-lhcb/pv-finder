9fe83e69fbba73b6a50ef5c2e5eb715bf50eea4b
Mike Sokoloff <mike.sokoloff@uc.edu>
2021-02-17 10:41:22 -0500

    updated model for Tracks_To_KDE that uses Will's new data loader + file with relevant method

--- model/collectdata_kdeB_Ellipsoids.py
+++ model/collectdata_kdeB_Ellipsoids.py
@@ -286,4 +286,11 @@ def collect_truth(*files, pvs=True):
         concatenate(n_list),
         concatenate(c_list),
     )
-
+def load_prebuilt_dataset(batch_size=16, num_files=1):
+    tracks, KDEs, PVs = [], [], []
+    for i in range(1, num_files+1):
+        tracks.append(torch.Tensor(np.load(f'/share/lazy/will/data/tracks_to_KDE/June30_2020_80k_{i}_tracks.h5.npy')))
+        KDEs.append(torch.Tensor(np.load(f'/share/lazy/will/data/tracks_to_KDE/June30_2020_80k_{i}_KDEs.h5.npy')))
+        PVs.append(torch.Tensor(np.load(f'/share/lazy/will/data/tracks_to_KDE/June30_2020_80k_{i}_PVs.h5.npy')))
+## mds     return torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.cat(tracks), torch.cat(KDEs), torch.cat(PVs)), batch_size=batch_size)
+    return torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.cat(tracks), torch.cat(KDEs) ), batch_size=batch_size)
