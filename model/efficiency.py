09ca7d3fbfe26e1204b7021701efcc5e74d86917
Elliot Kaufman <ekauffma@sleepy.geop.uc.edu>
2021-07-29 09:08:49 -0400

    changes to target hist generation

--- model/efficiency.py
+++ model/efficiency.py
@@ -95,6 +95,8 @@ def pv_locations(targets, threshold, integral_threshold, min_width):
     # Special case for final item (very rare or never occuring)
     # handled by above if len
 
+    print('nitems: ', nitems)
+    
     return items[:nitems]
 
 
--- model/origdata_mds_poca_KDE.py
+++ model/origdata_mds_poca_KDE.py
@@ -209,12 +209,13 @@ def process_root_file(filepath, sd_1=0.1):
 ##        X = (tree["oldzdata"].array() / 50.0).astype(dtype_X)  # Density in z, 4000xN
 ## in origdata_mdsA (for original KDEs) we divided by 2500
 ## Oops! That was wrong scaling factor.  Try dividing by 6500 rather than 2500
-        X = (tree["oldzdata"].array() / 2500.0).astype(dtype_X)  # Density in z, 4000xN
-        Xmax = (tree["oldxmax"].array() / 2500.0).astype(
+        X = (tree["oldzdata"].array() / 2500.0).astype(dtype_X)  # Density in z, 4000xN # EMK change scaling factor
+        print(X.shape)
+        Xmax = (tree["oldxmax"].array()).astype(
             dtype_X
         )  # Location of max z in x   <OPTIONAL>
 ## mds 10 Sept 2020        Ymax = (tree["ymax"].array() / 2500.0).astype(
-        Ymax = (tree["oldymax"].array() / 2500.0).astype(
+        Ymax = (tree["oldymax"].array()).astype(
             dtype_X
         )  # Location of max z in y   <OPTIONAL>
         Xmax[X == 0] = 0
@@ -280,37 +281,41 @@ def process_root_file(filepath, sd_1=0.1):
         sv_cat.content = sv_cat.content.astype(np.int8)
 
     N_vals = len(X)
-    zvals_range = (-99.95, 299.95)
-    Y = np.zeros([4, N_vals, 4000], dtype=dtype_Y)
-    edges = np.array([-0.05, 0.05])
-    ##    bins = np.arange(-3, 4)
-    bins = np.arange(-5, 6)
-    mat = 0.1 * bins[np.newaxis, :] + edges[:, np.newaxis] - 99.95
+    n_bins = 10000
+    zmin = -25.0
+    zmax = 25.0
+    bin_width = (zmax-zmin)/n_bins
+    zvals_range = (zmin+bin_width/2, zmax-bin_width/2)
+    Y = np.zeros([4, N_vals, n_bins], dtype=dtype_Y)
+    edges = np.array([-bin_width/2, bin_width/2])
+    bins = np.arange(-10, 11)
+    mat = bin_width * bins[np.newaxis, :] + edges[:, np.newaxis] + zvals_range[0]
 
     msgs = []
     with Timer(start=f"Processing events: {name}"):
         for i in range(N_vals):
             columns = (
-                (pv_loc[i][pv_cat[i] == 1], pv_ntrks[i][pv_cat[i] == 1]),
-                (pv_loc[i][pv_cat[i] != 1], pv_ntrks[i][pv_cat[i] != 1]),
-                (sv_loc[i][sv_cat[i] == 1], sv_ntrks[i][sv_cat[i] == 1]),
-                (sv_loc[i][sv_cat[i] != 1], sv_ntrks[i][sv_cat[i] != 1]),
+                ## below zeros bc currently no category (everything zeros)
+                (pv_loc[i][pv_cat[i] >= 0], pv_ntrks[i][pv_cat[i] >= 0]),
+                (pv_loc[i][pv_cat[i] < 0], pv_ntrks[i][pv_cat[i] < 0]),
+                (sv_loc[i][sv_cat[i] >= 0], sv_ntrks[i][sv_cat[i] >= 0]),
+                (sv_loc[i][sv_cat[i] < 0], sv_ntrks[i][sv_cat[i] < 0]),
             )
 
-            if mds_counter < 10:
+#             if mds_counter < 10:
 
-                print(" \n \n N_vals = ", N_vals)
+#                 print(" \n \n N_vals = ", N_vals)
 
-                print("len(columns) = ", len(columns))
-                print("columns = \n", columns)
-                print("columns[0][0] = ", columns[0][0])
-                print("columns[0][1] = ", columns[0][1])
-                print("columns[1][0] = ", columns[1][0])
-                print("columns[1][1] = ", columns[1][1])
-                print("columns[2][0] = ", columns[2][0])
-                print("columns[2][1] = ", columns[2][1])
-                print("columns[3][0] = ", columns[3][0])
-                print("columns[3][1] = ", columns[3][1])
+#                 print("len(columns) = ", len(columns))
+#                 print("columns = \n", columns)
+#                 print("columns[0][0] = ", columns[0][0])
+#                 print("columns[0][1] = ", columns[0][1])
+#                 print("columns[1][0] = ", columns[1][0])
+#                 print("columns[1][1] = ", columns[1][1])
+#                 print("columns[2][0] = ", columns[2][0])
+#                 print("columns[2][1] = ", columns[2][1])
+#                 print("columns[3][0] = ", columns[3][0])
+#                 print("columns[3][1] = ", columns[3][1])
             mds_counter += 1
             ##            assert(mds_counter)<10
 
@@ -322,16 +327,16 @@ def process_root_file(filepath, sd_1=0.1):
                 ## mds            sd = 0.001*pvRes  ## convert from microns (in TDR) to  mm (units used here)
 
                 entries = len(elements[0])
-                if mds_counter < 10:
-                    print(" \n \n \n elements =  ", elements)
-                    print(" entries = ", entries)
+#                 if mds_counter < 10:
+#                     print(" \n \n \n elements =  ", elements)
+#                     print(" entries = ", entries)
                 for v_index in range(entries):
                     centers = elements[0][v_index]
                     nTrks = elements[1][v_index]
                     sd = np.where(
                         nTrks < 4,
                         sd_1,
-                        0.001 * (A_res * np.power(nTrks, -1 * B_res) + C_res),
+                        0.01 * (A_res * np.power(nTrks, -1 * B_res) + C_res),
                     )
 
                     test = (zvals_range[0] < centers) & (centers < zvals_range[1])
@@ -340,39 +345,39 @@ def process_root_file(filepath, sd_1=0.1):
                     nTrks = nTrks[test]
                     sd = sd[test]
 
-                    if mds_counter < 10:
-                        print("\n \n n = ", n)
-                        print(" \n centers = ", centers)
-                        print(" nTrks  =       ", nTrks)
-                        print(" sd     =       ", sd)
+#                     if mds_counter < 10:
+#                         print("\n \n n = ", n)
+#                         print(" \n centers = ", centers)
+#                         print(" nTrks  =       ", nTrks)
+#                         print(" sd     =       ", sd)
 
                     for mean, ntrk, pv_res in zip(centers, nTrks, sd):
-                        if mds_counter < 10:
-                            print(" \n iterating  using zip(centers, nTrks, sd)  ")
-                            print(" \n  mean = ", mean)
-                            print(" ntrk   = ", ntrk)
-                            print(" pv_res = ", pv_res)
+#                         if mds_counter < 10:
+#                             print(" \n iterating  using zip(centers, nTrks, sd)  ")
+#                             print(" \n  mean = ", mean)
+#                             print(" ntrk   = ", ntrk)
+#                             print(" pv_res = ", pv_res)
 
                         # Compute bin number
-                        N_bin = int(np.floor((mean - zvals_range[0]) * 10))
-                        values = norm_cdf(mean, pv_res, N_bin / 10 + mat)
+                        N_bin = int(np.floor((mean - zvals_range[0]) /bin_width))
+                        values = norm_cdf(mean, pv_res, N_bin *bin_width + mat)
                         ##  replace sd_1 with sd computed from nTrks
                         ##  and increase area to be inversely proportional to sd for sd <150 microns
                         ##  (which is roughly the resolution expected for nTrks ~ 10) and with
                         ##  with unit area at sd = 0.150 mm = 150 micron, and larger values (lower nTrk)
                         ##  altogether, this will make the target peaks more narrow and the areas larger as the
                         ##  number of tracks increases.
-                        if mds_counter < 10:
-                            print("N_bin = ", N_bin)
-                            print("mat = ", mat)
-                            print("  values = ", values)
+#                         if mds_counter < 10:
+#                             print("N_bin = ", N_bin)
+#                             print("mat = ", mat)
+#                             print("  values = ", values)
 
                         populate = values[1] - values[0]
                         populate = np.where(
                             (0.15 / pv_res) > 1, (0.15 / pv_res) * populate, populate
                         )
-                        if mds_counter < 10:
-                            print(" populate = ", populate)
+#                         if mds_counter < 10:
+#                             print(" populate = ", populate)
 
                         try:
                             ## mds                    Y[n, i, bins + N_bin] += values[1] - values[0]
