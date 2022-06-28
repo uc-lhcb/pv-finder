813ed6a64a497e326f640f17a8a26d481f66566f
Elliott Kauffman <ekauffma@cern.ch>
2022-06-28 14:02:00 -0400

    fixed prt_pvr

--- ana/data/datapvs.h
+++ ana/data/datapvs.h
@@ -19,6 +19,8 @@ template <class T> struct DataPVs2IO : public CoreIO<T, float> {
     Vec pvr_y{this, "pvr_y"};
     Vec ntrks_prompt{this, "ntrks_prompt"};
     Vec ntrks{this, "ntrks"};
+    Vec nverts_before{this, "nverts_before"};
+    Vec nverts_after{this, "nverts_after"};
 
     Vec sv_cat{this, "sv_cat"};
     Vec svr_z{this, "svr_z"};
@@ -50,11 +52,8 @@ inline void copy_in_pvs2(DataPVsOut2 &self,
 
     self.clear();
     
-    std::vector< double > pvr_x_new;
-    std::vector< double > pvr_y_new;
-    std::vector< double > pvr_z_new;
-    std::vector< int > ntrks_prompt_new;
-    std::vector< int > ntrks_new;
+    // initialize new prt_pvr and fill with -1 (initially no assignment)
+    std::vector<int> prt_pvr_new(data_trks.prt_pvr->size(),-1);
         
     std::vector< double > pvr_x_current;
     std::vector< double > pvr_y_current;
@@ -63,8 +62,8 @@ inline void copy_in_pvs2(DataPVsOut2 &self,
     std::vector< int > current_numtracks;
     std::vector< int > current_ntrks_prompt;
     
-    std::vector<int> good_pointers;
-    std::vector<int> reassigned_pointers;
+//     std::vector<int> good_pointers;
+//     std::vector<int> reassigned_pointers;
     
     int trklen = 0;
     int sumofweights = 0;
@@ -83,12 +82,22 @@ inline void copy_in_pvs2(DataPVsOut2 &self,
         
         double first_pvr_z = data_pvs.pvr_z->at(counter);
         int current_ind = i;
+        
+        int nv_before = 0;
+        int nv_after = 0;
         while (current_ind == i) {
             
             trklen = 0;
             
-            good_pointers.push_back(counter);
-            reassigned_pointers.push_back(current_ind);
+//             good_pointers.push_back(counter);
+//             reassigned_pointers.push_back(current_ind);
+            nv_before++;
+            
+            for (int k = 0; k<data_trks.prt_pvr->size(); ++k) {
+                if (data_trks.prt_pvr->at(k)==counter){
+                    prt_pvr_new.at(k) = current_ind;
+                }
+            }
             
             if (data_pvs.pvr_n_source_tracks->at(counter) == 0 && 
                 data_pvs.pvr_process_type->at(counter) == 0 &&
@@ -102,10 +111,8 @@ inline void copy_in_pvs2(DataPVsOut2 &self,
                         ++trklen;
                     }
                 }
+                nv_after++;
                 current_numtracks.push_back(trklen);
-                
-//                 good_pointers.push_back(counter);
-//                 reassigned_pointers.push_back(current_ind);
             }
             
             counter += 1;
@@ -132,55 +139,36 @@ inline void copy_in_pvs2(DataPVsOut2 &self,
                 weightedavg_z+=current_numtracks.at(k)*pvr_z_current.at(k)/sumofweights;
             }
             
-            ntrks_new.push_back(std::accumulate(current_numtracks.begin(),
-                                                current_numtracks.end(),
-                                                decltype(current_numtracks)::value_type(0)));
+            self.ntrks->push_back(std::accumulate(current_numtracks.begin(),
+                                                 current_numtracks.end(),
+                                                 decltype(current_numtracks)::value_type(0)));
             
-            pvr_x_new.push_back(weightedavg_x);
-            pvr_y_new.push_back(weightedavg_y);
-            pvr_z_new.push_back(weightedavg_z);
+            self.pvr_x->push_back(weightedavg_x);
+            self.pvr_y->push_back(weightedavg_y);
+            self.pvr_z->push_back(weightedavg_z);
             
-            ntrks_prompt_new.push_back(std::accumulate(current_ntrks_prompt.begin(),
-                                                       current_ntrks_prompt.end(),
-                                                       decltype(current_ntrks_prompt)::value_type(0)));
+            self.nverts_before->push_back(nv_before);
+            self.nverts_after->push_back(nv_after);
+            
+            self.ntrks_prompt->push_back(std::accumulate(current_ntrks_prompt.begin(),
+                                                        current_ntrks_prompt.end(),
+                                                        decltype(current_ntrks_prompt)::value_type(0)));
         }
     }
     
      // iterate through truth tracks and construct new prt_pvr
-    for(int i = 0; i < data_trks.prt_pvr->size(); ++i) {
-        // check if current value is in "good pointer"
-        auto it = find(good_pointers.begin(), good_pointers.end(), data_trks.prt_pvr->at(i));
-        if (it!=good_pointers.end()) {
-            int index = it - good_pointers.begin();
-            self.prt_pvr->push_back(reassigned_pointers.at(index));
-        }
-        else {
-            self.prt_pvr->push_back(-1);
-        }
-    }
-    
-    for(int i = 0; i < pvr_z_new.size(); ++i) {
-        self.pvr_z->push_back(pvr_z_new.at(i));
-        self.pvr_x->push_back(pvr_x_new.at(i));
-        self.pvr_y->push_back(pvr_y_new.at(i));
-        self.ntrks->push_back(ntrks_new.at(i));
+    for(int i = 0; i < prt_pvr_new.size(); ++i) {
+        self.prt_pvr->push_back(prt_pvr_new.at(i));
     }
     
     for(int i = 0; i < data_hits.prt_hits->size(); ++i) {
         self.prt_hits->push_back(data_hits.prt_hits->at(i));
     }
     
-    for(int i = 0; i < ntrks_prompt_new.size(); ++i) {
-        self.ntrks_prompt->push_back(ntrks_prompt_new.at(i));
-    }
-
     for(int i = 0; i < data_pvs.svr_z->size(); ++i) {
-//         self.sv_cat->push_back(svCategory(data_trks, data_pvs, data_hits, i));
         self.svr_z->push_back(data_pvs.svr_z->at(i));
         self.svr_x->push_back(data_pvs.svr_x->at(i));
         self.svr_y->push_back(data_pvs.svr_y->at(i));
-//         self.svr_pvr->push_back(data_pvs.svr_pvr->at(i));
-//         self.sv_ntrks->push_back(nSVPrt(data_trks, data_pvs, data_hits, i));
     }
     
     for(int i = 0; i < data_trks.prt_x->size(); ++i) {
@@ -213,8 +201,6 @@ template <class T> struct DataPVsIO : public CoreIO<T, float> {
     Vec sv_loc_x{this, "sv_loc_x"};
     Vec sv_loc_y{this, "sv_loc_y"};
     Vec sv_ntrks{this, "sv_ntrks"};
-
-    // sv_n and pv_n are no longer present
 };
 
 using DataPVsOut = DataPVsIO<Out>;
